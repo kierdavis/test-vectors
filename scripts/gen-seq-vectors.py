3ef0f95178b64d1e9fa3b0f3a61ca77e255b317a
Kier Davis <kierdavis@gmail.com>
2017-05-08 10:49:06 +0100

    Fix not having accounted for the frequency divider in the sequencer test vectors

--- scripts/gen-seq-vectors.py
+++ scripts/gen-seq-vectors.py
@@ -26,20 +26,24 @@ print_row("C", 1, 0, [0,0,0,1,0], 0, 0)
 print_row("C", 1, 0, [0,0,1,0,0], 1, 0)
 
 digit1 = digit2 = digit3 = digit4 = 0
+n = 2
 
 for i in range(num_vectors/4):
-  digit4 += 1
-  if digit4 > 9:
-    digit4 = 0
-    digit3 += 1
-    if digit3 > 9:
-      digit3 = 0
-      digit2 += 1
-      if digit2 > 5:
-        digit2 = 0
-        digit1 += 1
-        if digit1 > 9:
-          digit1 = 0
+  n += 1
+  if n > 4:
+    n = 0
+    digit4 += 1
+    if digit4 > 9:
+      digit4 = 0
+      digit3 += 1
+      if digit3 > 9:
+        digit3 = 0
+        digit2 += 1
+        if digit2 > 5:
+          digit2 = 0
+          digit1 += 1
+          if digit1 > 9:
+            digit1 = 0
   print "# stopwatch display = %d%d%d%d" % (digit1, digit2, digit3, digit4)
   print_row("C", 1, 1, [1,0,0,0,0], 1, digit1)
   print_row("C", 1, 1, [0,1,0,0,0], 0, digit2)
